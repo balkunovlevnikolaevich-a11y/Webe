b2b50fc6d2791522e55008b63b899b0593029006
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-18 21:53:45 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,17 +3,19 @@ from app.models import User, Task, Category, TaskStatus
 
 app = create_app()
 
-if __name__ == '__main__':
-    with app.app_context():
-        # Создаем таблицы
-        db.create_all()
-        
-        # Категории
-        if not Category.query.first():
-            categories = ['Веб-разработка', 'Дизайн', 'Тексты', 'Маркетинг', 'Прочее']
-            for cat_name in categories:
-                db.session.add(Category(name=cat_name))
-            db.session.commit()
-            print("✅ Категории созданы!")
+# ВЫНОСИМ ЭТО ИЗ-ПОД IF, чтобы Gunicorn это увидел
+with app.app_context():
+    # Создаем таблицы (если их еще нет)
+    db.create_all()
+    
+    # Наполняем категориями
+    if not Category.query.first():
+        categories = ['Веб-разработка', 'Дизайн', 'Тексты', 'Маркетинг', 'Прочее']
+        for cat_name in categories:
+            db.session.add(Category(name=cat_name))
+        db.session.commit()
+        print("✅ База данных и категории инициализированы!")
 
+# Этот блок теперь нужен только для локальной разработки
+if __name__ == '__main__':
     app.run(debug=True)
